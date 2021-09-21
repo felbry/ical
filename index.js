9a118abe0ea741637ac92c2f631de0f3b190f66b
chaifangbo <505792925@qq.com>
2021-09-21 11:39:53 +0800

    .

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import ical from 'ical-generator'
+import { getVtimezoneComponent } from '@touch4it/ical-timezones'
 import moment from 'moment'
 
 const calendar = ical({
-  name: 'my first iCal',
+  name: '国内节假日',
+})
+calendar.timezone({
+  name: 'FOO',
+  generator: getVtimezoneComponent,
 })
 calendar.createEvent({
-  start: moment(),
-  end: moment().add(1, 'hour'),
-  summary: 'Example Event',
-  description: 'It works ;)',
-  location: 'my room',
-  url: 'http://sebbo.net/',
+  summary: '中秋节',
+  // start: moment.tz('2021-09-19 00:00', 'Asia/Shanghai').format(),
+  // end: moment.tz('2021-09-19 00:00', 'Asia/Shanghai').add(3, 'days').format(),
+  start: moment('2021-09-19 00:00'),
+  end: moment('2021-09-19 00:00').add(3, 'days'),
+  timezone: 'Asia/Shanghai',
 })
-calendar.saveSync('./calendar.ical')
+calendar.createEvent({
+  summary: '国庆节',
+  start: moment('2021-10-01 00:00'),
+  end: moment('2021-10-01 00:00').add(7, 'days'),
+  timezone: 'Asia/Shanghai',
+})
+calendar.saveSync('./calendar.ics')
